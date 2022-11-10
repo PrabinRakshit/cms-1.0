dea716e7c12a2211c7172f5007dff9a124ca2053
PrabinRakshit <iamprabinrakshit@gmail.com>
2022-11-10 15:20:28 +0530

    updated client build

---
+++ client/src/component/Default/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="caontainer">
+        <div className="row">
+            <div className="col-md-12 text-center">
+                <h3 className="display-3 text-success">Contact</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
